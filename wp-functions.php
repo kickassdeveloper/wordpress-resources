a5f0e8a5231f5db36dcfb724842328836c4bb1b7
Miroslav Ostojic <imortalart@gmail.com>
2016-11-25 17:38:40 +0100

    is_ssl()
    Added is_ssl() support

--- wp-functions.php
+++ wp-functions.php
@@ -1,11 +1,13 @@
 <?php
 function set_https($string){
-    $test = strpos($string, 'https');
-    if($test === false){
-         $result = str_replace('http', 'https', $string);
-         return $result;
-    }else{
-    return $string;
-    }
+	if (is_ssl()) {
+		$test = strpos($string, 'https');
+		if($test === false){
+			$result = str_replace('http', 'https', $string);
+			return $result;
+		}else{
+			return $string;
+		}
+	}
 }
 ?>
