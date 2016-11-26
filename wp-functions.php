9028f54ff97216b5d145f3dfa2ca625033c8651c
Miroslav Ostojic <imortalart@gmail.com>
2016-11-26 16:24:06 +0100

    Fix bug/add comments
    Fix bug/add comments

--- wp-functions.php
+++ wp-functions.php
@@ -1,9 +1,17 @@
 <?php
-function set_https($string){
+// Function for transforming urls from http to https
+// The function checks if the ssl is activate, 
+// else returns the original string.
+// If the ssl is active it ckecks if the link is https,
+// if it is returns it, else it converts the link https,
+// and then returns it.
+
+function set_https($string){// $string is the passing argument
 	if (is_ssl()) {
-		$test = strpos($string, 'https');
+		$test = strpos($string, 'https:');//checks if the link has https in it, if so it returns true
 		if($test === false){
-			$result = str_replace('http', 'https', $string);
+			$result = str_replace('http:', 'https:', $string);
+			//swaps the http: , with https: (   ":"" is for so i  you can use http://example.org/how-http-works/' without transforming the secon http to https)
 			return $result;
 		}else{
 			return $string;
