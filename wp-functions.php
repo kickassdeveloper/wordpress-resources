42ea29c8b000075c2c2d093933d8571c0da1a8a0
Miroslav Ostojic <imortalart@gmail.com>
2017-04-23 18:40:28 +0200

    New fucntion register custom taxonomy

--- wp-functions.php
+++ wp-functions.php
@@ -182,3 +182,17 @@ function get_custom_posts($post_type){
     $custom_posts = new WP_Query($args);
     return $custom_posts;
 }
+
+//register new custom type taxonomy( category)
+function add_custom_types_to_tax( $query ) {
+if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+// Get all your post types
+$post_types = get_post_types();
+
+$query->set( 'post_type', $post_types );
+return $query;
+}
+}
+add_filter( 'pre_get_posts', 'add_custom_types_to_tax' );
+?>
