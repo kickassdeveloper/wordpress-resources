1bc56987474fa9bc54c0bfcb4480cad1982d0966
Miroslav Ostojic <imortalart@gmail.com>
2018-09-16 15:06:37 +0200

    Create js_injection_plugin.php

---
+++ js_injection_plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Specific JS injection
+Plugin URI: https://veles.tech
+Description: Adds JS code to the <footer> of a theme, by hooking to wp_footer.
+Author: Miroslav Ostojic
+Version: 1.0
+ */
+
+function inject_js() {
+global $wp;
+$current_url = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
+if($current_url == 'url zeljene stranice stranice'){
+	echo '<script src="'. esc_url( plugins_url( 'images/wordpress.png', __FILE__ ) ).'/imeJSfajlaGdeSeNalazi.js"></script>';
+ 	 }
+}
+add_action( 'wp_footer', 'inject_js', 10 );
